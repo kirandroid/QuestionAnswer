4c5abce445adac8df9c52cf1ca88ed444869ede1
geekmiester <justforstudy777@gmail.com>
2019-01-04 23:24:48 +0545

    welcome page

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View ,Image, Button,TouchableOpacity, Alert} from "react-native";
 
 export default class App extends Component {
   render() {
+    
     return (
+      
       <View style={styles.container}>
-        <View style={styles.navContainer} />
+        <View style={styles.navContainer} >
+        <Image
+          style={{flex:1,resizeMode:'contain',width:355, height:210}}
+          source={require('./Resource/Design/welcome.png')}/>
+        </View>
         <View style={styles.bodyContainer} >
-        <Text>New Text</Text></View>
-        <View style={styles.bottomContainer} />
+        <Text style={styles.text}>WELCOME</Text>
+        <TouchableOpacity onPress = {showAlert} style = {styles.button}>
+         <Text style={styles.txtcolor}>Get Started</Text>
+      </TouchableOpacity>
+        </View>
+        <View style={styles.bottomContainer}>
+        
+        
+          <View style={styles.bottomrows}>
+        <Image
+          style={{borderLeftWidth:200, borderTopWidth:250, width:310, height:170}}
+          source={require('./Resource/Design/welele1.png')}/>
+          </View>      
+        </View>
       </View>
     );
   }
 }
 
+const showAlert = () =>{
+  Alert.alert(
+     'Kiran Boka'
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,19 +44,47 @@ const styles = StyleSheet.create({
   },
 
   navContainer: {
-    flex: 1,
-    elevation: 3,
+    flex: 6,
     backgroundColor: "#FFFFFF",
     height: 50
   },
 
   bodyContainer: {
-    flex: 9,
+    flex:2,
     backgroundColor: "#FFFFFF"
   },
 
   bottomContainer: {
-    flex: 1,
-    backgroundColor: "#FFFFFF"
-  }
+    flex: 4,
+    backgroundColor: "#FFFFFF",
+    alignItems: 'flex-end',
+  },
+  bottomrows:{
+    flexDirection:'row',
+    flex:2,
+    alignItems:'center'
+  },
+  button: {
+    left:65,
+    backgroundColor: '#E91E63',
+    width: 230,
+    height:50,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent:'space-around'  
+ },
+ text:{
+   fontSize:30,
+   left:100,
+   fontFamily:'Bariol',
+   fontWeight:'bold',
+   alignItems:'center',
+   justifyContent:'space-between'
+ },
+ txtcolor:{
+   color:'white',
+   fontSize:20,
+   fontFamily:'Helvetica',
+   fontWeight:'bold',
+ }
 });
