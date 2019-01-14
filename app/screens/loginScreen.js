1967c461a5aaa3ca4f684544375b639704cad650
geekmiester <justforstudy777@gmail.com>
2019-01-14 23:04:31 +0545

    LoginScreen.js complete

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button,Image, TouchableOpacity } from 'react-native'
 import firebase from 'react-native-firebase';
 
 export default class loginScreen extends React.Component {
@@ -13,8 +13,13 @@ export default class loginScreen extends React.Component {
     }
     render() {
         return (
+            <View style={styles.maincontainer}>
+            <View style={styles.imagecontainer}>
+            <Image
+                style={{flex:1,resizeMode:'center',width:353, height:210}}
+                source={require('/home/nibba/Desktop/questionanswer/Resource/Design/login.png')}/>
+            </View>
             <View style={styles.container}>
-                <Text>Login</Text>
                 {this.state.errorMessage &&
                     <Text style={{ color: 'red' }}>
                         {this.state.errorMessage}
@@ -34,7 +39,11 @@ export default class loginScreen extends React.Component {
                     onChangeText={password => this.setState({ password })}
                     value={this.state.password}
                 />
-                <Button title="Login" onPress={this.handleLogin} />
+                <TouchableOpacity onPress={this._handleLogin} style={styles.button}>
+      <Text style={styles.textstyle}>LOGIN</Text>
+    </TouchableOpacity>
+                {/* <Button style={styles.button} title="Login"  onPress={this.handleLogin} /> */}
+            </View>
             </View>
         )
     }
@@ -46,10 +55,40 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     textInput: {
+        
         height: 40,
         width: '90%',
+        height: '19%',
         borderColor: 'gray',
-        borderWidth: 1,
-        marginTop: 8
+        borderWidth: 2,
+        borderRadius:25,
+        marginTop:8
+    },
+    maincontainer:{
+        flex:1,
+        flexDirection:'column',
+        justifyContent:"space-between",
+        backgroundColor:'#FFFFFF'
+    },
+    imagecontainer:{
+        flex:1,
+        flexDirection:'column',
+        justifyContent:"space-around",
+        backgroundColor:'#FFFFFF'
+    },
+    textstyle:{
+            color:'white',
+            fontSize:21,
+            fontFamily:'Helvetica',
+            fontWeight:'bold'
+    },
+    button:{
+        marginTop:8,
+        backgroundColor: '#B3CF47',
+        paddingLeft:70,
+        paddingRight:70,
+        paddingTop:10,
+        paddingBottom:10,
+        borderRadius: 80,  
     }
 })
